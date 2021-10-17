5da10a0b433f785410a01b33613b7d1e1f86bf09
narges <itsnrgr@gmail.com>
2021-10-17 18:40:57 +0330

    last update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 let apiKey = "be81f193e065bf5feb2d944c7336968b";
 let city = "tehran";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
-function displayTemperature (){
+function displayTemperature (response){
 let temperatureElement = document.querySelector("#current-temperature");
  celsiusTemperature = response.data.main.temp;
 
@@ -14,12 +14,34 @@ let temperatureElement = document.querySelector("#current-temperature");
   state.innerHTML = response.data.weather[0].description;
   let cityElement=document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
+    let iconElement = document.querySelector("#icon");
 
+    iconElement.setAttribute(
+      "src",
 
-
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
 
 };
 
+
+
+function searchCity (city) {
+let apiKey = "be81f193e065bf5feb2d944c7336968b";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmitButton (event){
+    event.preventDefault();
+    let cityElement =document.querySelector("#input-city").value;
+    searchCity(cityElement);
+}
+let searchForm = document.querySelector("#search-form");
+
+searchForm.addEventListener("submit", handleSubmitButton);
+
+searchCity(city);
 let hour = now.getHours();
 let minute = now.getMinutes();
 let time = document.querySelector("#time");
@@ -36,20 +58,3 @@ let days = [
 let day = days[now.getDay()];
 let date = document.querySelector("#day");
 date.innerHTML = ` ${day}`;
-
-function searchCity (city) {
-let apiKey = "be81f193e065bf5feb2d944c7336968b";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(displayTemperature);
-}
-function handleSubmitButton (event){
-    event.preventDefault();
-    let cityElement =document.querySelector("#input-city").value;
-    search(cityElement);
-}
-let searchForm = document.querySelector("#search-form");
-
-searchForm.addEventListener("submit", handleSubmitButton);
-
-searchCity(city);
