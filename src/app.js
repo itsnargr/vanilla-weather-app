f858f97e3dcfcf9e5ea0670dcc3ea73f56ab5ed9
narges <itsnrgr@gmail.com>
2021-10-22 20:24:47 +0330

    last one

--- src/app.js
+++ src/app.js
@@ -42,6 +42,7 @@ let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmitButton);
 
 searchCity(city);
+let now = new Date();
 let hour = now.getHours();
 let minute = now.getMinutes();
 let time = document.querySelector("#time");
@@ -58,3 +59,44 @@ let days = [
 let day = days[now.getDay()];
 let date = document.querySelector("#day");
 date.innerHTML = ` ${day}`;
+function showDailyForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+  <div class="day">${formatDays(forecastDay.dt)}</div>
+                <img
+                  src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
+                  alt=""
+                  width="50"
+                />
+              <div class="forecast-temperature">
+                <span class="forecast-temp-max">${Math.round(
+                  forecastDay.temp.max
+                )}<span class="degrees-symbol-max">°</span>
+                </span>
+                <span class="forecast-temp-min">${Math.round(
+                  forecastDay.temp.min
+                )}<span class="degrees-symbol-min">°</span>
+                </span>
+              </div>
+              </div>`;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "be81f193e065bf5feb2d944c7336968b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showDailyForecast);
+}
